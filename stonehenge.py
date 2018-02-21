7c01e8a56dc94f34cdd357b6e1d5cefdb9e8d623
Mit <mit_rupang@yahoo.ca>
2018-02-21 01:39:36 -0500

    Added methods

--- stonehenge.py
+++ stonehenge.py
@@ -3,12 +3,45 @@ An implementation of the Stonehenge game and its state.
 """
 from game import Game
 from game_state import GameState
+from typing import Any
 
 
 class StonehengeGame(Game):
     """
     The Stonehenge game.
     """
+    def __init__(self, p1_starts: bool) -> None:
+        """
+        Initialize this Game, using p1_starts to find who the first player is.
+        """
+        raise NotImplementedError
+
+    def get_instructions(self) -> str:
+        """
+        Return the instructions for this Game.
+        """
+        raise NotImplementedError
+
+    def is_over(self, state: GameState) -> bool:
+        """
+        Return whether or not this game is over at state.
+        """
+        raise NotImplementedError
+
+    def is_winner(self, player: str) -> bool:
+        """
+        Return whether player has won the game.
+
+        Precondition: player is 'p1' or 'p2'.
+        """
+        raise NotImplementedError
+
+    def str_to_move(self, string: str) -> Any:
+        """
+        Return the move that string represents. If string is not a move,
+        return some invalid move.
+        """
+        raise NotImplementedError
 
 
 class StonehengeState(GameState):
