ccf2ea41a30bd4840aebbe649490f1701187e6be
Mit <mit_rupang@yahoo.ca>
2018-02-22 22:05:56 -0500

    Revised the data structure for the board and added a method to extract ley-lines

--- stonehenge.py
+++ stonehenge.py
@@ -3,24 +3,7 @@ An implementation of the Stonehenge game and its state.
 """
 from game import Game
 from game_state import GameState
-from typing import Any, List, Optional, Union
-
-
-def create_board_dict(n: int) -> List[List[Optional[str]]]:
-    """
-    Create a board of size n.
-    """
-    letters = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
-    number_of_cells = int((((n + 1) * (n + 2)) / 2) - 1 + n)
-    cells = letters[:number_of_cells]
-    list_return = []
-    for i in range(2, n + 2):
-        temp = []
-        for j in range(i):
-            temp.append(cells.pop(0))
-        list_return.append(temp)
-    list_return.append(cells)
-    return list_return
+from typing import Any, List, Dict, Optional, Union
 
 
 def score(line: List[Union[str, int]]) -> Optional[int]:
@@ -38,16 +21,15 @@ class StonehengeGame(Game):
     """
     The Stonehenge game.
 
-    p1_starts - the starting player of the game
+    size - the side length of the board
     current_state - the current state of the game
     """
     def __init__(self, p1_starts: bool) -> None:
         """
         Initialize this Game, using p1_starts to find who the first player is.
         """
-        board = create_board_dict(int(input('Enter the side length '
-                                            'of the board: ')))
-        self.current_state = StonehengeState(p1_starts, board)
+        self.size = int(input('Enter the side length of the board: '))
+        self.current_state = StonehengeState(p1_starts, self.create_cells())
 
     def get_instructions(self) -> str:
         """
@@ -84,28 +66,50 @@ class StonehengeGame(Game):
         Return the move that string represents. If string is not a move,
         return some invalid move.
         """
-        move = string.strip()
-        letters = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
-        n = len(self.current_state.board[0]) - 1
-        number_of_cells = int((((n + 1) * (n + 2)) / 2) - 1 + n)
-        cells = letters[:number_of_cells]
-        if move in cells:
-            return move
+        if type(string) is str:
+            move = string.strip()
+            cells = self.create_cells()
+            if move in cells:
+                return move
         return None
+
+    def create_cells(self) -> List[str]:
+        """
+        Create a board of size self.size.
+        """
+        n = self.size
+        letters = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
+        number_of_cells = int(((n ** 2) + 5 * n) / 2)
+        return letters[:number_of_cells]
 
 
 class StonehengeState(GameState):
     """
     The state of Stonehenge at a certain point in time.
+
+    size - the side length of the board
+    cells - the cells in the board, which can be an alphabetical character
+    or 1 or 2
+    board -
     """
-    def __init__(self, is_p1_turn: bool, board: List[List[Union[str, int, None]]], cells: str) -> None:
+    def __init__(self, is_p1_turn: bool, cells: List[Union[str, int]]) -> None:
         """
         Initialize this game state and set the current player based on
         is_p1_turn.
         """
         super().__init__(is_p1_turn)
+        if len(cells) == 3:
+            self.size = 1
+        elif len(cells) == 7:
+            self.size = 2
+        elif len(cells) == 12:
+            self.size = 3
+        elif len(cells) == 18:
+            self.size = 4
+        elif len(cells) == 25:
+            self.size = 5
         self.cells = cells
-        # self.board = self.create_board()
+        self.board = self.create_board()
 
     def __str__(self) -> str:
         """
@@ -113,11 +117,11 @@ class StonehengeState(GameState):
         """
         raise NotImplementedError
 
-    def get_possible_moves(self) -> list:
+    def get_possible_moves(self) -> List[str]:
         """
         Return all possible moves that can be applied to this state.
         """
-        raise NotImplementedError
+        return [cell for cell in self.cells if type(cell) is str]
 
     def make_move(self, move: Any) -> 'GameState':
         """
@@ -130,7 +134,7 @@ class StonehengeState(GameState):
         Return a representation of this state (which can be used for
         equality testing).
         """
-        raise NotImplementedError
+        return str(self) + "\nP1's turn: {}".format(self.p1_turn)
 
     def rough_outcome(self) -> float:
         """
@@ -139,11 +143,46 @@ class StonehengeState(GameState):
         """
         raise NotImplementedError
 
-    def get_ley_lines(self) -> List[List[Union[str, int, None]]]:
+    def create_board(self) -> List[Dict[Union[str, int], List[Union[str, int]]]]:
+        """
+        Create a board of size n.
+        """
+        # ley_lines = self.get_ley_lines()
+
+    def get_ley_lines(self) -> List[List[Union[str, int]]]:
         """
         Return a list of all the ley-lines in the board.
         """
-        pass
+        ley_lines = self.create_rows()
+        rows = ley_lines[:]
+
+        # Extracting the / ley-lines
+        for i in range(self.size + 1):
+            temp_list = []
+            for j in range(len(rows)):
+                if j != len(rows) - 1:
+                    try:
+                        temp_list.append(rows[j][i])
+                    except IndexError:
+                        pass
+                else:
+                    if i != 0:
+                        temp_list.append(rows[j][i-1])
+            ley_lines.append(temp_list)
+        return ley_lines
+
+    def create_rows(self) -> List[List[Union[str, int]]]:
+        """
+        Create the rows (horizontal ley-lines) from self.cells.
+        """
+        n = self.size
+        list_return = []
+        temp = 0
+        for i in range(2, n + 2):
+            list_return.append(self.cells[temp:temp + i])
+            temp += i
+        list_return.append(self.cells[len(self.cells) - n:])
+        return list_return
 
 
 if __name__ == "__main__":
